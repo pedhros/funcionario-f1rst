22332f714d0c5aa90edc8cbd971ab5e1ff48f280
Pedroh <pedrooohsss@gmail.com>
2024-06-06 21:21:37 -0300

    inicia o projeto

---
+++ src/main/java/br/com/f1rst/funcionario_f1rst/FuncionarioF1rstApplication.java
@@ -0,0 +1,13 @@
+package br.com.f1rst.funcionario_f1rst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioF1rstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioF1rstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/f1rst/funcionario_f1rst/FuncionarioF1rstApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.f1rst.funcionario_f1rst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioF1rstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
