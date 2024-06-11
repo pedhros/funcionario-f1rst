a673a70b087b5c405b1b7d6c0470855c3865ad73
Pedroh <pedrooohsss@gmail.com>
2024-06-11 19:55:04 -0300

    implementa api de teste

--- src/main/java/br/com/f1rst/funcionario_f1rst/FuncionarioF1rstApplication.java
+++ src/main/java/br/com/f1rst/funcionario_f1rst/FuncionarioF1rstApplication.java
@@ -2,10 +2,19 @@ package br.com.f1rst.funcionario_f1rst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping ("/")
 public class FuncionarioF1rstApplication {
 
+	@GetMapping
+	public String getHomeTest() {
+		return "Funcionario F1rst - API Home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FuncionarioF1rstApplication.class, args);
 	}
